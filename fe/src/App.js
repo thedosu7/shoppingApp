b9053d806fe26ef8be819c17aa052e0b3db2a0ce
Pham Duc <thedosu2001@gmail.com>
2023-03-25 14:22:57 +0700

    Create React App

---
+++ fe/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Shopping App</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
