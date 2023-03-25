058a1702d384ce30da63dc36eda83a8f4202eaed
Pham Duc <thedosu2001@gmail.com>
2023-03-25 15:33:50 +0700

    Product List

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,22 @@
+import data from './data';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="/">Shopping App</a>
       </header>
-      <main>List products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <image src={product.image} alt={product.name} />
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
