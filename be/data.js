8b2d4f47ca43a6477b0d520d33256785b9d70cf2
Pham Duc <thedosu2001@gmail.com>
2023-04-18 02:20:39 +0700

    Add Sample User And Products

--- be/data.js
+++ be/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Duc',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Pham',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
--- be/server.js
+++ be/server.js
@@ -23,22 +23,22 @@ app.get('/api/products', (req, res) => {
 });
 
 // the backend API for returning product information based on the slug of the product.
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
+// app.get('/api/products/slug/:slug', (req, res) => {
+//   const product = data.products.find((x) => x.slug === req.params.slug);
+//   if (product) {
+//     res.send(product);
+//   } else {
+//     res.status(404).send({ message: 'Product Not Found' });
+//   }
+// });
+// app.get('/api/products/:id', (req, res) => {
+//   const product = data.products.find((x) => x._id === req.params.id);
+//   if (product) {
+//     res.send(product);
+//   } else {
+//     res.status(404).send({ message: 'Product Not Found' });
+//   }
+// });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server is running at https://localhost:${port}`);
