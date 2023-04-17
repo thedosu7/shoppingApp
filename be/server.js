dd26b8da23b45371c9a78755f9686f8eaf8575d5
Pham Duc <thedosu2001@gmail.com>
2023-04-18 02:53:11 +0700

    Signin API

--- be/server.js
+++ be/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import seedRouter from './routes/seedRoute.js';
 import productRouter from './routes/productRoute.js';
+import userRouter from './routes/userRoute.js';
 
 dotenv.config();
 mongoose
@@ -16,29 +17,16 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
+app.use('/api/users', userRouter);
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
-// the backend API for returning product information based on the slug of the product.
-// app.get('/api/products/slug/:slug', (req, res) => {
-//   const product = data.products.find((x) => x.slug === req.params.slug);
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: 'Product Not Found' });
-//   }
-// });
-// app.get('/api/products/:id', (req, res) => {
-//   const product = data.products.find((x) => x._id === req.params.id);
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: 'Product Not Found' });
-//   }
-// });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server is running at https://localhost:${port}`);
